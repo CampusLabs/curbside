de91c6803b52b1b9e25adabc3574b3168ac456d9
Casey Foster <c@sey.me>
2017-06-30 16:49:13 -0500

    The simplest check

--- src/check.js
+++ src/check.js
@@ -1,28 +1,29 @@
-require('./initializers/set-config-from-stdin');
-
-const _ = require('underscore');
-const config = require('./config');
-const fetch = require('node-fetch');
-
-const {
-  concourse: {pipeline, resource, team},
-  curbside: {url},
-  resource: {version: {id}},
-  webhookToken
-} = config;
-
-(async () => {
-  try {
-    const res = await fetch(
-      `${url}/builds/${team}/${pipeline}/${resource}${id ? `/${id}` : ''}` +
-      `?andNewer&webhookToken=${webhookToken}`
-    );
-    const builds = await res.json();
-    if (builds.error) throw new Error(builds.error);
-
-    console.log(JSON.stringify(_.map(builds, ({id}) => ({id}))));
-  } catch (er) {
-    console.error(er);
-    process.exit(1);
-  }
-})();
+// require('./initializers/set-config-from-stdin');
+//
+// const _ = require('underscore');
+// const config = require('./config');
+// const fetch = require('node-fetch');
+//
+// const {
+//   concourse: {pipeline, resource, team},
+//   curbside: {url},
+//   resource: {version: {id}},
+//   webhookToken
+// } = config;
+//
+// (async () => {
+//   try {
+//     const res = await fetch(
+//       `${url}/builds/${team}/${pipeline}/${resource}${id ? `/${id}` : ''}` +
+//       `?andNewer&webhookToken=${webhookToken}`
+//     );
+//     const builds = await res.json();
+//     if (builds.error) throw new Error(builds.error);
+//
+//     console.log(JSON.stringify(_.map(builds, ({id}) => ({id}))));
+//   } catch (er) {
+//     console.error(er);
+//     process.exit(1);
+//   }
+// })();
+console.log([]);
