06d4a4cc856771f378cbd15305b3592629703d64
Casey Foster <c@sey.me>
2017-07-01 16:17:35 -0500

    Try to log stdin

--- src/utils/get-stdin-json.js
+++ src/utils/get-stdin-json.js
@@ -2,7 +2,7 @@ module.exports = () =>
   new Promise(resolve => {
     let json = '';
     process.stdin.on('data', data => {
-      console.error(data);
+      console.error(json);
       try { resolve(JSON.parse(json += data.toString())); } catch (er) {}
     });
   });
