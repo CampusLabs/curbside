38b5c86d3da2e9852cca566c90e70a7a17666b70
Casey Foster <c@sey.me>
2017-07-02 06:30:53 -0500

    Fix set-config-from-file

--- src/initializers/set-config-from-file.js
+++ src/initializers/set-config-from-file.js
@@ -1,6 +1,6 @@
 const _ = require('underscore');
+const config = require('../config');
 const fs = require('fs');
-const config = require('../utils/set-config');
 
 module.exports = () =>
   _.extend(config, JSON.parse(fs.readFileSync('./curbside/config')));
